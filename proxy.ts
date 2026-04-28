d8937d3d3e117aaafaef6cdc26c87fac9c0108e4
Herman <hsfnelissen@gmail.com>
2026-04-28 14:00:22 +0200

    upgrade next

--- proxy.ts
+++ proxy.ts
@@ -9,7 +9,7 @@ const PROTECTED_PREFIXES = [
   "/instellingen",
 ];
 
-export function middleware(request: NextRequest) {
+export function proxy(request: NextRequest) {
   const { pathname } = request.nextUrl;
 
   const isProtected = PROTECTED_PREFIXES.some((p) => pathname.startsWith(p));
